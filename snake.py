d5beb19329ed2bf0ef8dd2463eeddf154ea2e2a5
Pratik <146824256+Raip036@users.noreply.github.com>
2023-12-05 19:01:13 +0000

    Version 1.0

---
+++ snake.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from constants import *
+
+import random
+
+class Snake:
+    pass
+
+
+
+
+
+
+
+class Food:
+    pass
+
+
+def next_turn():
+    pass
+
+def change_direction():
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
